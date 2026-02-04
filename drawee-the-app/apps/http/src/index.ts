4a5fde91796111aae06d7db7a1d0580b2b6275a8
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2026-02-04 20:44:57 +0530

    added cors to http backend

--- drawee-the-app/apps/http/src/index.ts
+++ drawee-the-app/apps/http/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { HTTP_PORT } from "@repo/backend-common/config";
 
 const app = express();
 const PORT = HTTP_PORT;
 
 app.use(express.json());
+app.use(cors());
 
 import authRouter from "./router/auth.routes.js";
 import roomRouter from "./router/room.routes.js";
